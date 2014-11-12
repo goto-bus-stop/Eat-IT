9355a6649d2439efebae9c10e5ee6beee09316b1
René Kooi <renekooi@outlook.com>
2014-11-12 15:04:40 +0100

    update install script

--- install.php
+++ install.php
@@ -5,6 +5,8 @@ require 'include.inc';
 $db = new MineTurtle\Database\PDOConnector('mysql:host=localhost;dbname=eatit', 'root', '');
 MineTurtle\Model::useDB($db);
 
+// tables
+
 $db->createTable('Artikel');
 $db->createTable('ArtikelOrder');
 $db->createTable('Ingredient');
@@ -16,10 +18,9 @@ $db->createTable('Maaltijd');
 $db->createTable('Order');
 $db->createTable('OrderRegel');
 
-// artikelen
-$a = array();
-$a[] = array (
-  'artikelnr' => '1',
+// data
+
+$a = new Artikel(array (
   'naam' => 'Test',
   'leverancier' => NULL,
   'voorraad' => '10',
@@ -27,9 +28,10 @@ $a[] = array (
   'besteld' => NULL,
   'prijs' => '100',
   'verkoop' => '1',
-);
-$a[] = array (
-  'artikelnr' => '2',
+));
+$a->save();
+
+$a = new Artikel(array (
   'naam' => 'niet verkoop',
   'leverancier' => NULL,
   'voorraad' => NULL,
@@ -37,9 +39,10 @@ $a[] = array (
   'besteld' => NULL,
   'prijs' => '43',
   'verkoop' => '0',
-);
-$a[] = array (
-  'artikelnr' => '3',
+));
+$a->save();
+
+$a = new Artikel(array (
   'naam' => 'Tilapiafilet',
   'leverancier' => NULL,
   'voorraad' => '27',
@@ -47,9 +50,10 @@ $a[] = array (
   'besteld' => '0',
   'prijs' => '4.15',
   'verkoop' => '0',
-);
-$a[] = array (
-  'artikelnr' => '4',
+));
+$a->save();
+
+$a = new Artikel(array (
   'naam' => 'Tortilla',
   'leverancier' => NULL,
   'voorraad' => '166',
@@ -57,9 +61,10 @@ $a[] = array (
   'besteld' => '0',
   'prijs' => '0.1',
   'verkoop' => '0',
-);
-$a[] = array (
-  'artikelnr' => '5',
+));
+$a->save();
+
+$a = new Artikel(array (
   'naam' => 'Chipotle-adobosaus',
   'leverancier' => NULL,
   'voorraad' => '78',
@@ -67,9 +72,10 @@ $a[] = array (
   'besteld' => '0',
   'prijs' => '0.3',
   'verkoop' => '0',
-);
-$a[] = array (
-  'artikelnr' => '6',
+));
+$a->save();
+
+$a = new Artikel(array (
   'naam' => 'IJsbergsla 100g',
   'leverancier' => NULL,
   'voorraad' => '254',
@@ -77,9 +83,10 @@ $a[] = array (
   'besteld' => '0',
   'prijs' => '0.7',
   'verkoop' => '0',
-);
-$a[] = array (
-  'artikelnr' => '7',
+));
+$a->save();
+
+$a = new Artikel(array (
   'naam' => 'Komkommer',
   'leverancier' => NULL,
   'voorraad' => '70',
@@ -87,9 +94,10 @@ $a[] = array (
   'besteld' => '0',
   'prijs' => '1.3',
   'verkoop' => '0',
-);
-$a[] = array (
-  'artikelnr' => '8',
+));
+$a->save();
+
+$a = new Artikel(array (
   'naam' => 'Tomaat',
   'leverancier' => NULL,
   'voorraad' => '120',
@@ -97,101 +105,87 @@ $a[] = array (
   'besteld' => '0',
   'prijs' => '0.2',
   'verkoop' => '0',
-);
+));
+$a->save();
 
-foreach ($a as $x) {
-  $b = new Artikel($x);
-  $b->save();
-}
-
-// Ingredienten
-$i = array();
-$i[] = array (
-  'ingedientnr' => '1',
-  'maaltijd' => '2',
-  'artikel' => '3',
-  'hoeveelheid' => '1',
-);
-$i[] = array (
-  'ingedientnr' => '2',
-  'maaltijd' => '2',
-  'artikel' => '4',
-  'hoeveelheid' => '1',
-);
-$i[] = array (
-  'ingedientnr' => '3',
-  'maaltijd' => '2',
-  'artikel' => '5',
-  'hoeveelheid' => '2',
-);
-$i[] = array (
-  'ingedientnr' => '4',
-  'maaltijd' => '3',
-  'artikel' => '6',
-  'hoeveelheid' => '3',
-);
-$i[] = array (
-  'ingedientnr' => '5',
-  'maaltijd' => '3',
-  'artikel' => '7',
-  'hoeveelheid' => '1',
-);
-$i[] = array (
-  'ingedientnr' => '6',
-  'maaltijd' => '3',
-  'artikel' => '8',
-  'hoeveelheid' => '2',
-);
-
-foreach ($i as $x) {
-  $b = new Ingredient($x);
-  $b->save();
-}
-
-// Maaltijden
-$m = array();
-$m[] = array (
-  'maaltijdnr' => '1',
-  'naam' => 'maaltijd',
-  'prijs' => '20.34',
-  'beschikbaar' => '7',
-  'daghap' => '0',
-  'img' => '',
-);
-$m[] = array (
-  'maaltijdnr' => '2',
-  'naam' => 'Gegrilde Vistaco met Chipotle/limoen-dressing',
+$a = new Maaltijd(array (
+  'naam' => 'Frisse Vistaco',
+  'omschrijving' => 'Gegrilde Vistaco met Chipotle/limoen-dressing',
   'prijs' => '12.99',
   'beschikbaar' => '20',
-  'daghap' => '0',
-  'img' => '',
-);
-$m[] = array (
-  'maaltijdnr' => '3',
-  'naam' => '"Tao Lin"-salade',
-  'prijs' => '5',
-  'beschikbaar' => '20',
-  'daghap' => '0',
-  'img' => '',
-);
-$m[] = array (
-  'maaltijdnr' => '4',
-  'naam' => '	Spinazie met zalm',
+  'daghap' => '1',
+  'img' => '2.jpg',
+  'type' => 'vis',
+));
+$a->save();
+
+$a = new Maaltijd(array (
+  'naam' => 'Spinazie met zalm',
+  'omschrijving' => 'Het is spinazie, en het is zalm.',
   'prijs' => '8.45',
   'beschikbaar' => '1',
   'daghap' => '0',
   'img' => 'spinazie.jpg',
-);
-$m[] = array (
-  'maaltijdnr' => '5',
+  'type' => 'vis',
+));
+$a->save();
+
+$a = new Maaltijd(array (
   'naam' => 'Broccoli met amandelen',
+  'omschrijving' => 'Niemand houdt van broccoli. Maar met amandelen is het niet eens zo heel erg.',
   'prijs' => '6.7',
   'beschikbaar' => '1',
   'daghap' => '0',
   'img' => 'broccoli.jpg',
-);
+  'type' => 'vega',
+));
+$a->save();
 
-foreach ($m as $x) {
-  $b = new Maaltijd($x);
-  $b->save();
-}
+$a = new Ingredient(array (
+  'ingedientnr' => '1',
+  'maaltijd' => '2',
+  'artikel' => '3',
+  'hoeveelheid' => '1',
+));
+$a->save();
+
+$a = new Ingredient(array (
+  'ingedientnr' => '2',
+  'maaltijd' => '2',
+  'artikel' => '4',
+  'hoeveelheid' => '1',
+));
+$a->save();
+
+$a = new Ingredient(array (
+  'ingedientnr' => '3',
+  'maaltijd' => '2',
+  'artikel' => '5',
+  'hoeveelheid' => '2',
+));
+$a->save();
+
+$a = new Ingredient(array (
+  'ingedientnr' => '4',
+  'maaltijd' => '3',
+  'artikel' => '6',
+  'hoeveelheid' => '3',
+));
+$a->save();
+
+$a = new Ingredient(array (
+  'ingedientnr' => '5',
+  'maaltijd' => '3',
+  'artikel' => '7',
+  'hoeveelheid' => '1',
+));
+$a->save();
+
+$a = new Ingredient(array (
+  'ingedientnr' => '6',
+  'maaltijd' => '3',
+  'artikel' => '8',
+  'hoeveelheid' => '2',
+));
+$a->save();
+
